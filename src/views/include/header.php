fa5ab59c0c8fd6c02dbb229e0cf18f12709c4148
Bryan Sanchez <bryanipsanchez@gmail.com>
2023-04-06 15:14:11 +0800

    Sign in page

--- src/views/include/header.php
+++ src/views/include/header.php
@@ -6,20 +6,22 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="<?= URLROOT; ?>/public/css/bootstrap.min.css" media="screen">
-
+	<link rel="stylesheet" href="<?= URLROOT; ?>/public/css/config.css" media="screen">
 	<title>
 		<?= SITENAME; ?>
 	</title>
 </head>
 
 <body>
-	<nav>
-		<ul>
-			<li><a href="<?= URLROOT; ?>"> Home </a></li>
-			<li><a href="<?= URLROOT; ?>/about"> About </a></li>
-			<li><a href="<?= URLROOT; ?>/test/tasks">Test database</a></li>
-			<li><a href="<?= URLROOT; ?>/auth/signup">Sign up</a></li>
-			<li><a href="<?= URLROOT; ?>/auth/signin">Sign in</a></li>
-			<li><a href="<?= URLROOT; ?>/auth/logout">Log out</a></li>
-		</ul>
-	</nav>
+	<?php if (isset($_SESSION['user_id'])): ?>
+		<nav>
+			<ul>
+				<li><a href="<?= URLROOT; ?>"> Home </a></li>
+				<li><a href="<?= URLROOT; ?>/about"> About </a></li>
+				<li><a href="<?= URLROOT; ?>/test/tasks">Test database</a></li>
+				<li><a href="<?= URLROOT; ?>/auth/signup">Sign up</a></li>
+				<li><a href="<?= URLROOT; ?>/auth/signin">Sign in</a></li>
+				<li><a href="<?= URLROOT; ?>/auth/logout">Log out</a></li>
+			</ul>
+		</nav>
+	<?php endif; ?>
