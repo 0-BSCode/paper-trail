2531af7ba5f7dac0cf67a9f8599fe8b41c4ed63d
Bryan Sanchez <bryanipsanchez@gmail.com>
2023-04-07 19:05:19 +0800

    Initialized view ticket page

--- src/views/Tickets/view.php
+++ src/views/Tickets/view.php
@@ -1,5 +1,30 @@
 <?php require_once APPROOT . '/src/views/include/header.php'; ?>
-<pre>
-    <?php print_r($data); ?>
-</pre>
+<main>
+    <h1>
+        View Ticket
+    </h1>
+    <?php if ($data): ?>
+        <form>
+            <div class="mb-3">
+                <label for="title" class="form-label">Title</label>
+                <input type="text" name="title" class="form-control" id="title" placeholder="Juan de la Cruz" disabled
+                    value="<?= $data['title']; ?>">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Category</label>
+                <select class="form-select form-select-md mb-3" name="category_id" aria-label=".form-select-lg example"
+                    disabled>
+                    <option selected value="<?= $data['category_id']; ?>"><?= $data['name']; ?></option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Example textarea</label>
+                <textarea class="form-control" name="description" id="description" rows="10"
+                    disabled><?= $data['description']; ?></textarea>
+            </div>
+        </form>
+    <?php else: ?>
+        <p>Error fetching data.</p>
+    <?php endif; ?>
+</main>
 <?php require_once APPROOT . '/src/views/include/footer.php'; ?>
