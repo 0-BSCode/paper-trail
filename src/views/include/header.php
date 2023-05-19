19aa9c4f870a6e5297285a4f5b51bea64be82b37
Bryan Sanchez <bryanipsanchez@gmail.com>
2023-05-19 21:24:27 +0800

    Refactored navbar design

--- src/views/include/header.php
+++ src/views/include/header.php
@@ -15,24 +15,33 @@
 
 <body>
 	<?php if (isset($_SESSION['user_id'])): ?>
-		<nav class="d-flex justify-content-between align-items-start">
-			<ul class="list-group d-flex flex-row list-group-flush">
-				<?php if ($_SESSION['role'] === 'student'): ?>
-					<li class="list-group-item"><a href="<?= URLROOT; ?>"> Home </a></li>
-				<?php else: ?>
-					<li class="list-group-item"><a href="<?= URLROOT; ?>">Grievances</a></li>
-					<li class="list-group-item"><a href="<?= URLROOT; ?>/contact/index">Contacts</a></li>
-					<li class="list-group-item"><a href="<?= URLROOT; ?>/document/index">Documents</a></li>
-				<?php endif; ?>
-			</ul>
-			<ul class="list-group d-flex flex-row list-group-flush">
-				<li class="list-group-item">
-					<img src="<?= URLROOT; ?>/public/assets/images/person-circle.svg" alt="Profile Pic">
-					<span>
-						<?= $_SESSION['user_name']; ?>
-					</span>
-				</li>
-				<li class="list-group-item"><a href="<?= URLROOT; ?>/auth/logout">Log out</a></li>
-			</ul>
+		<nav class="navbar navbar-expand-md navbar-dark bg-secondary">
+			<div class="container-fluid">
+				<div class="d-flex justify-content-start">
+					<?php if ($_SESSION['role'] === 'student'): ?>
+						<a href="<?= URLROOT; ?>" class="navbar-brand">
+							<h4 class="mb-0">Home</h4>
+						</a>
+					<?php else: ?>
+						<a href="<?= URLROOT; ?>" class="navbar-brand">
+							<h4 class="mb-0">Grievances</h4>
+						</a>
+						<a href="<?= URLROOT; ?>/contact/index" class="navbar-brand">
+							<h4 class="mb-0">Contacts</h4>
+						</a>
+						<a href="<?= URLROOT; ?>/document/index" class="navbar-brand">
+							<h4 class="mb-0">Documents</h4>
+						</a>
+					<?php endif; ?>
+				</div>
+				<div class="d-flex justify-content-end">
+					<div class="d-flex align-items-center me-3">
+						<h6 class="mb-0 text-white">
+							<?= $_SESSION['user_name']; ?>
+						</h6>
+					</div>
+					<a href="<?= URLROOT; ?>/auth/logout" class="btn btn-danger">Log out</a>
+				</div>
+			</div>
 		</nav>
 	<?php endif; ?>
