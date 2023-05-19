147a48fbdf207eeaa28ca44f8386402b9d87c3a7
alexabainza <abainza.alexandra@gmail.com>
2023-05-19 14:28:28 +0800

    Fixed merge conflicts

--- src/views/Documents/view.php
+++ src/views/Documents/view.php
@@ -1,48 +1,43 @@
 <?php require_once APPROOT . '/src/views/include/header.php'; ?>
-<main>
-  <h1>School Memorandum and Significant Documents</h1>
-  <p>
-    <a href="<?= URLROOT; ?>/document/create" button class="btn btn-success">Add a new document</button></a>
-  </p>
-  <table class="table">
-    <thead>
+<main class = "mx-5 my-3">
+    <h1>School Memorandum and Significant Documents</h1>
+    <p>
+        <a href = "<?= URLROOT; ?>/document/create" button class="btn btn-success">Add a new document</button></a>
+    </p>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Document ID</th>
+      
+      <th scope="col">Name</th>
+      <th scope="col">Link to Document</th>
+      <th scope="col">Description</th>
+      <th scope="col">Date Created</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($data['documents'] as $document){?>
       <tr>
-        <th scope="col">Document ID</th>
-        <th scope="col">Name</th>
-        <th scope="col">Link to Document</th>
-        <th scope="col">Description</th>
-        <th scope="col">Date Created</th>
-        <th scope="col">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($data['documents'] as $document) { ?>
-        <tr>
-          <th scope="row">
-            <?= $document->document_id; ?>
-          </th>
+        <th scope="row">
+          <?= $document->document_id;?>
+        </th>
           <td scope="row">
             <a href="<?= URLROOT; ?>/document/<?= $document->document_id; ?>/view-document-one">
-              <?= $document->name; ?></a>
+                <?= $document->name; ?></a>
           </td>
-          <td scope="row">
-            <?= $document->link; ?>
-          </td>
-          <td scope="row">
-            <?= $document->description; ?>
-          </td>
-          <td scope="row">
-            <?= date(DATE_FORMAT, strtotime($document->date_created)); ?>
-          </td>
+          <td scope="row"><?php echo $document->link?></td>
+          <td scope="row"><?php echo $document->description?></td>
+          <td scope="row"><?php echo $document->date_created?></td>
           <td>
-            <form method="post" action="<?= URLROOT; ?>/document/delete">
-              <input type="hidden" name="document_id" value="<?php echo $document->document_id ?>">
-              <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+            <form method="post" action = "<?= URLROOT; ?>/document/delete">
+              <input type = "hidden" name="document_id" value="<?php echo $document->document_id?>">
+              <button type = "submit" class="btn btn-sm btn-outline-danger">Delete</button>
             </form>
           </td>
-        </tr>
-      <?php } ?>
+      </tr>
+    <?php }?>
     </tbody>
-  </table>
+    </table>
 </main>
 <?php require_once APPROOT . '/src/views/include/footer.php'; ?>
