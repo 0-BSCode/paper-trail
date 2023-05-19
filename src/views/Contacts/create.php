c8b1a1cf25150351b71eed327faaeea312ec4919
Bryan Sanchez <bryanipsanchez@gmail.com>
2023-05-19 21:33:08 +0800

    Refactored create forms

--- src/views/Contacts/create.php
+++ src/views/Contacts/create.php
@@ -3,19 +3,19 @@
 <main class="mx-5 my-3">
     <h1>Create Contact</h1>
     <form action="<?= URLROOT; ?>/contact/create-contact" method="POST">
-        <div class="form-group">
+        <div class="form-grou mb-3">
             <label>First Name</label>
             <input type="text" class="form-control" name="first_name" placeholder="Enter first name of contact">
         </div>
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label>Last Name</label>
             <input type="text" class="form-control" name="last_name" placeholder="Enter last name of contact">
         </div>
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label>Email</label>
             <input type="email" class="form-control" name="email" placeholder="Enter email of contact">
         </div>
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label>Contact Number</label>
             <input type="number" class="form-control" name="contact_no" placeholder="Enter contact number">
         </div>
--- src/views/Documents/create.php
+++ src/views/Documents/create.php
@@ -1,18 +1,18 @@
 <?php require_once APPROOT . '/src/views/include/header.php'; ?>
-<main>
+<main class="mx-5 my-3">
     <h1>Add New Document</h1>
-    <form action = "<?= URLROOT; ?>/document/create-document" method = "POST">
-        <div class="form-group">
+    <form action="<?= URLROOT; ?>/document/create-document" method="POST">
+        <div class="form-group mb-3">
             <label>Document Name</label>
-            <input type ="text" class="form-control" name = "name" placeholder="Enter title of document">
+            <input type="text" class="form-control" name="name" placeholder="Enter title of document">
         </div>
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label>Link to announcement</label>
-            <input type ="url" class="form-control" name = "link" placeholder="Enter link where you found the post">
+            <input type="url" class="form-control" name="link" placeholder="Enter link where you found the post">
         </div>
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label>Description</label>
-            <textarea class="form-control" name = "description" placeholder="Enter short description"></textarea>
+            <textarea class="form-control" name="description" placeholder="Enter short description"></textarea>
         </div>
         <br>
         <button type="submit" class="btn btn-primary">Submit</button>
