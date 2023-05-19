c006fa82b7d85557246f12290ea81cdc79cbbebe
Bryan Sanchez <bryanipsanchez@gmail.com>
2023-05-19 21:31:16 +0800

    Refactored table

--- src/views/Documents/index.php
+++ src/views/Documents/index.php
@@ -8,9 +8,7 @@
     <thead>
       <tr>
         <th scope="col">Document ID</th>
-
         <th scope="col">Name</th>
-        <th scope="col">Link to Document</th>
         <th scope="col">Description</th>
         <th scope="col">Date Created</th>
         <th scope="col">Action</th>
@@ -27,13 +25,10 @@
               <?= $document->name; ?></a>
           </td>
           <td scope="row">
-            <?php echo $document->link ?>
+            <?= $document->description ?>
           </td>
           <td scope="row">
-            <?php echo $document->description ?>
-          </td>
-          <td scope="row">
-            <?php echo $document->date_created ?>
+            <?= date(DATE_FORMAT, strtotime($document->date_created)); ?>
           </td>
           <td>
             <form method="post" action="<?= URLROOT; ?>/document/delete-document">
