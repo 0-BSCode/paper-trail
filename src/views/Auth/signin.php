4f63722d0f1089d7922a332a17ba18db7d15dc89
Bryan Sanchez <bryanipsanchez@gmail.com>
2023-05-19 21:38:07 +0800

    Refactored signin form

--- src/views/Auth/signin.php
+++ src/views/Auth/signin.php
@@ -2,16 +2,16 @@
 <main class="h-100 d-flex flex-column px-5 justify-content-center align-items-center">
     <h1>Sign in</h1>
     <form action="<?= URLROOT; ?>/auth/signin" method="post">
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label for="email">Email address</label>
             <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"
                 name="email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                else.</small>
         </div>
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label for="password">Password</label>
             <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
+                else.</small>
         </div>
         <div class="d-flex flex-column align-items-center">
             <button type="submit" class="btn btn-primary">Log in</button>
